1b2e1052f38d1d0935dbff1fe0f9c4903198071f
Gurpreet Singh <er.gurpreet.2017@gmail.com>
2020-05-19 16:33:40 -0400

    everything uptodate witout migrations

--- RateItData/Review.cs
+++ RateItData/Review.cs
@@ -12,15 +12,20 @@ namespace RateItData
     {
         [Key]
         public int ReviewId { get; set; }
+
         [Required]
         public Guid OwnerId { get; set; }
+
         [Required]
         public string Content { get; set; }
+
         [Required]
         [Range(1, 5, ErrorMessage = "Enter a rating on a scale of 1-5")]
         public int Rating { get; set; }
+
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
+
         public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
